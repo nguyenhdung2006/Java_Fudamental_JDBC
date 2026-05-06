8d30545f6fb388451c67a217d5650958aa3ade0b
HuyDung <nguyenhdung2006@gmail.com>
2026-05-06 15:40:49 +0700

    Fix Gioi1 content

--- src/Gioi1.java
+++ src/Gioi1.java
@@ -1,28 +1,21 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Gioi1 {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        System.out.print("Nhập password: ");
+        String password = sc.nextLine();
 
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "abcdefghijklmnopqrstuvwxyz"
-                + "0123456789";
+        boolean isValid = password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,}$");
 
-        Random random = new Random();
-
-        StringBuilder result = new StringBuilder(n);
-
-        for (int i = 0; i < n; i++) {
-            int index = random.nextInt(chars.length());
-            result.append(chars.charAt(index));
+        if (isValid) {
+            System.out.println("Valid password.");
+        } else {
+            System.out.println("Invalid password.");
         }
-
-        System.out.println(result.toString());
 
         sc.close();
     }
--- src/Gioi2.java
+++ src/Gioi2.java
@@ -1,6 +1,29 @@
-public class Gioi2 {
+import java.util.Random;
+import java.util.Scanner;
 
-    public static void main() {
+public class Gioi1 {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "abcdefghijklmnopqrstuvwxyz"
+                + "0123456789";
+
+        Random random = new Random();
+
+        StringBuilder result = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = random.nextInt(chars.length());
+            result.append(chars.charAt(index));
+        }
+
+        System.out.println(result.toString());
+
+        sc.close();
     }
 }
