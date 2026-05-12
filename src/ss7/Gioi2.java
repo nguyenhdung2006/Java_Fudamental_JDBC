99eff5c97b7be7ebb5ae59e387379602f9c85600
HuyDung <nguyenhdung2006@gmail.com>
2026-05-13 02:59:28 +0700

    ss7 gioi 2

---
+++ src/ss7/Gioi2.java
@@ -0,0 +1,15 @@
+package ss7;
+
+public class Gioi2 {
+
+    public String title;
+    public String author;
+    public double price;
+
+    // Phương thức
+    public void printInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
