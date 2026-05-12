ecd226d5e61f2e965eb17fe37fe9e8c46f8f2512
HuyDung <nguyenhdung2006@gmail.com>
2026-05-13 03:06:57 +0700

    xs2

---
+++ src/ss7/xs2/Course.java
@@ -0,0 +1,11 @@
+package ss7.xs2;
+
+public class Course {
+
+    public String code;
+    public String title;
+
+    public void showCourse() {
+
+    }
+}
---
+++ src/ss7/xs2/Student.java
@@ -0,0 +1,12 @@
+package ss7.xs2;
+
+public class Student {
+
+    public int id;
+    public String name;
+    public Course course;
+
+    public void displayInfo() {
+
+    }
+}
